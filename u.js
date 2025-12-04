b948ef2adfc34b9660986eeaccf0651265ef31d3
koushalya <koushalyamalammanavark@gmail.com>
2025-12-04 16:27:55 +0530

    ka

--- u.js
+++
@@ -1,9 +0,0 @@
-function showLogin() {
-            document.getElementById("contactPage").classList.remove("active");
-            document.getElementById("loginPage").classList.add("active");
-        }
-
-        function showContact() {
-            document.getElementById("loginPage").classList.remove("active");
-            document.getElementById("contactPage").classList.add("active");
-        }
