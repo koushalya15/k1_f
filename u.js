a908fd2a3cd1892437ae30d2cf98b6d11d20feae
koushalya15 <koushalyamalammanark@gamil.com>
2025-12-04 11:01:41 +0530

    kou

---
+++ u.js
@@ -0,0 +1,9 @@
+function showLogin() {
+            document.getElementById("contactPage").classList.remove("active");
+            document.getElementById("loginPage").classList.add("active");
+        }
+
+        function showContact() {
+            document.getElementById("loginPage").classList.remove("active");
+            document.getElementById("contactPage").classList.add("active");
+        }
